dc8d980bf0b144493fad71a58430c7e178a859d8
adityaexe <kmradi2002@gmail.com>
2025-11-06 16:47:23 +0530

    refactor: simplify App component to render Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import React from "react";
+import Home from "./pages/Home.jsx";
 
-function App() {
-  return <div>App</div>;
+export default function App() {
+  return <Home />;
 }
-
-export default App;
